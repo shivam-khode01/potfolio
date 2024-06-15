ca77f57bbba16693cda38908bd8b8959625a2624
shivam khode <130055547+shivam-khode01@users.noreply.github.com>
2024-06-15 13:24:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 document.addEventListener('DOMContentLoaded', () => {
   let caseBtn = document.getElementById('spotifybtn');
   function projectRedirect() {
-      window.location.href ='https://shivam-khode01.github.io/potfolio-spotify/index.html';
+      window.location.replace='https://shivam-khode01.github.io/potfolio-spotify/index.html';
   }
   if (caseBtn) {
       caseBtn.addEventListener('click', projectRedirect);
